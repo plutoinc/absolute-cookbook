ba3dd5845bd614f81f9256647a2146173eb87179
am2rican5 <am2rican5@gmail.com>
2017-09-03 14:25:23 +0900

    [WIP] configure geth docker

--- geth/recipes/default.rb
+++ geth/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+docker_image 'ethereum/client-go' do
+  tag 'alpine'
+  action :pull
+end
+
+docker_container 'geth' do
+  repo 'ethereum/client-go'
+  tag 'alpine'
+end
