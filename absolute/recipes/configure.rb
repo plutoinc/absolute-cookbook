4dca634c9c8dce7dcf050fb721b87f129aa29449
am2rican5 <am2rican5@gmail.com>
2017-09-13 19:36:02 +0900

    nginx configuration

---
+++ absolute/recipes/configure.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: absolute
+# Recipe:: configure
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+Chef::Log.info('Absolute application configure script.')
+
+file '/etc/nginx/nginx.conf' do
+  action :delete
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
