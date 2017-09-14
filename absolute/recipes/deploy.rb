ee00ca51983860d6a36f79299c0f2f5d52fb89b1
am2rican5 <am2rican5@gmail.com>
2017-09-14 13:17:23 +0900

    running jar file

--- absolute/recipes/deploy.rb
+++ absolute/recipes/deploy.rb
@@ -9,7 +9,7 @@ Chef::Log.info('Absolute application deployment script.')
 app = search('aws_opsworks_app').first
 app_path = "/srv/#{app['shortname']}"
 
-Chef::Log.info('Clonning repository from github.')
+Chef::Log.info("Clonning repository from github into #{app_path}.")
 git app_path do
   repository app['app_source']['url']
   revision app['app_source']['revision']
@@ -19,7 +19,7 @@ end
 
 Chef::Log.info('Building the application using gradle.')
 execute './gradlew build' do
-  cwd "#{app_path}"
+  cwd app_path
   user 'root'
   command './gradlew build'
   action :run
@@ -28,4 +28,12 @@ end
 Chef::Log.info('Starting nginx...')
 service 'nginx' do
   action :start
+end
+
+Chef::Log.info('Starting application...')
+execute 'java -jar -Dspring.profiles.active=dev -Djava.security.egd=file:/dev/./urandom -jar build/libs/absolute-0.0.1.jar' do
+  cwd app_path
+  user 'root'
+  command 'java -jar -Dspring.profiles.active=dev -Djava.security.egd=file:/dev/./urandom -jar build/libs/absolute-0.0.1.jar'
+  action :run
 end
